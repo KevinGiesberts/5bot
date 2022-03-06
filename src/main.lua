a83dd8d6347cf8631ac3aeaadacc0ddeac420392
Fl1pNatic <newfl1pnatic@gmail.com>
2022-03-06 14:27:41 +0600

    a few thigns

--- src/main.lua
+++ src/main.lua
@@ -2,22 +2,58 @@ local env = require('envVars')
 local discordia = require('discordia')
 local client = discordia.Client()
 
+discordia.extensions()
+
+local jamCol = 0xFFBC80 -- Jam color, use for the `color` paremeter of the embed
+
 client:on('ready', function()
-	print('Logged in as '.. client.user.username)
+	print('Logged in as '.. client.user.username .. "#" .. client.user.discriminator)
 end)
 
 client:on('messageCreate', function(message)
     if message.author.bot then return end
-
     if string.sub(message.content, 1, 2) == "5!" then -- Check for prefix "5!"
         local cmd = message.content:sub(3) -- Remove prefix "5!"
-        print(cmd)
-
+        
         -- Commands will be here for now
-            if cmd == 'ping' then
-                message.channel:send('Pong!')
+            if cmd == 'jamlink' then
+                message:reply {
+                    embed = {
+                        title = "5YLJ Link",
+                        url = "https://itch.io/jam/5-year-long-jam",
+                        color = jamCol
+                    }
+                }
             end
+
+            if cmd == 'help' then
+                message:reply {
+                    embed = {
+                        title = "Help",
+                        fields = {
+                            {
+                                name = "Commands",
+                                value = "`jamlink` - Get the link to the 5YL Jam\n`help` - Get this message\n`avatar` - Get your own avatar",
+                                inline = true
+                            }
+                        },
+                        footer = { text = "Prefix: 5!" },
+                        color = jamCol
+                    }
+                }
+            end   
+            
+            if cmd == 'avatar' then 
+                message:reply {
+                    embed = {
+                        title = "Your Avatar",
+                        image = { url = message.author.avatarURL } ,
+                        color = jamCol
+                    }
+                }
+            end
+
 	end
 end)
 
-client:run("Bot ".. env.TOKEN) -- You need the "Bot" prefix before the token to login btwluvi
+client:run("Bot ".. env.TOKEN) -- You need the "Bot" prefix before the token to login btw
